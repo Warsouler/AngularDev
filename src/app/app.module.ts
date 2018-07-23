7defb482d7a676f72348ebd97f354d65bd1cf1fb
Juan-PC\Juan <juangroia_89@hotmail.com>
2018-07-23 14:14:18 -0300

    mi primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
+import { appRoutes } from './app.routing';
+
+
+
+
 import { AppComponent } from './app.component';
+import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
+import { Link1Component } from './link1/link1.component';
+import { Link2Component } from './link2/link2.component';
+import { Link3Component } from './link3/link3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    
+   
+    
+    
+    Link1Component,
+    Link2Component,
+    Link3Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

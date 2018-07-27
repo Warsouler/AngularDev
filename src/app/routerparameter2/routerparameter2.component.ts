41fc8cb12c2db3ec5281ed6641aba807f67d98c2
Juan-PC\Juan <juangroia_89@hotmail.com>
2018-07-27 20:31:12 -0300

    breadcum and details

--- src/app/routerparameter2/routerparameter2.component.ts
+++ src/app/routerparameter2/routerparameter2.component.ts
@@ -8,11 +8,36 @@ import { ActivatedRoute } from '../../../node_modules/@angular/router';
 })
 export class Routerparameter2Component implements OnInit {
 
+  lugares:any=[
+    {id:1,plan: "pagado",cercania:1, distancia: 1,active:true, nombre:'Floreria la Gardania',descripcion:'Esta es una descripcion nueva, mas adelante las completeamos 1'},
+    {id:2,plan: "gratuito",cercania:1, distancia: 1.8,active:false,nombre:'Donas la pasadita',descripcion:'Esta es una descripcion nueva, mas adelante las completeamos 2'},
+    {id:3,plan: "gratuito",cercania:2, distancia: 5,active:true, nombre:'Veterinaria Huellitas Felices',descripcion:'Esta es una descripcion nueva, mas adelante las completeamos 3'},
+    {id:4,plan: "pagado",cercania:3, distancia: 10,active:false, nombre:'Una cuarta',descripcion:'Esta es una descripcion nueva, mas adelante las completeamos 4'}
+  ];
+
   myid:number=0;
+  myref:string="";
+  myaction:string="";
+
+// declaramos una variable del tipo any osea cualquiera y la inicializamos vacia
+lugar:any={};
+
   constructor(private route: ActivatedRoute) {
     //  En la variable route.snapshot.params['parametro'] se encuentra el valor
-    alert(this.route.snapshot.params['id']);
-    this.myid=this.route.snapshot.params['id'];
+        this.myid=this.route.snapshot.params['id'];
+        // en la variable queryparams son los parametros opcionales
+        this.myref=this.route.snapshot.queryParams['referer'];
+        this.myaction=this.route.snapshot.queryParams['action'];
+
+        this.lugar=this.buscarLugar();
+
+    }
+    //declaramos una funcion que busque a través de la sentencia filter
+    // los lugares que matcheen con una condición, ponemos el [0] porque
+    // queremos que devuelva el primero, sino devuelve un array
+    // es una buena práctica poner el || null porque sino puede romper
+    buscarLugar(){
+      return this.lugares.filter((lugar)=>{return lugar.id==this.myid})[0] || null;
     }
  
   ngOnInit() {

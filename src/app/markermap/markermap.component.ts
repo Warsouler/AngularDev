84785fe3e6f7402d214d05c9536012d7d2c264d2
Juan <juangroia_89@hotmail.com>
2018-08-01 18:47:19 -0300

    ABC

--- src/app/markermap/markermap.component.ts
+++ src/app/markermap/markermap.component.ts
@@ -14,7 +14,7 @@ lng:number=-60.6657014;
   
 markmaps=[
   {id:1,name:"Proxit",lat:-32.9502445,lng:-60.6568437},
-  {id:2,name:"Rioja 2234",lat:-32.9446605,lng:-60.6563607},
+  {id:2,name:"hola2",lat:-32.9431515,lng:-60.6646717},
   {id:3,name:"hola3",lat:-32.9709368,lng:-60.6452035},
 
 ]
